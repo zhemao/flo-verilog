9537f915694c3ea0ebfa7cc18aab296c88bf0458
Howard Mao <zhehao.mao@gmail.com>
2014-08-22 12:10:21 -0700

    use getopt for option handling

--- src/flo2v.cpp
+++ src/flo2v.cpp
@@ -3,7 +3,7 @@
 #include <libflo/operation.h++>
 #include <libflo/version.h++>
 #include <libflo/sizet_printf.h++>
-#include <string.h>
+#include <getopt.h>
 
 #include "version.h"
 #include "libflo2v/generation.hpp"
@@ -12,26 +12,47 @@
 
 using namespace libflo;
 
-int main(int argc, const char **argv)
+static void print_help(const char *prog_name)
 {
-    /* Prints a simple help text if there isn't a sane set of
-     * command-line arguments, which in our case means just a single
-     * argument. */
-    if (argc <= 1 || argc > 2) {
-        std::cerr << argv[0] << " <flo>: generate verilog from a flo file\n";
-        exit(EXIT_FAILURE);
+    std::cerr << prog_name << " (--version | <flo>):"
+              << " generate verilog from a flo file\n";
+}
+
+int main(int argc, char *argv[])
+{
+    const struct option long_options[] = {
+        {"version", 0, NULL, 'v'},
+        {NULL, 0, NULL, 0}
+    };
+    int opt;
+    bool version = false;
+
+    while ((opt = getopt_long(argc, argv, "vd", long_options, NULL)) > 0) {
+        switch (opt) {
+        case 'v':
+            version = true;
+            break;
+        default:
+            print_help(argv[0]);
+            exit(EXIT_FAILURE);
+        }
     }
 
     /* If "--version" was passed then print out the version of this
      * program along with the version of libflo that this was linked
      * against. */
-    if (strcmp(argv[1], "--version") == 0) {
+    if (version) {
         std::cout << argv[0] << " " << PCONFIGURE_VERSION
                   << " (using libflo " << libflo::version() << ")\n";
         exit(0);
     }
 
-    auto flof = flo<node, operation<node> >::parse(argv[1]);
+    if (optind >= argc) {
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    auto flof = flo<node, operation<node> >::parse(argv[optind]);
 
     gen_flo(flof);
 }
