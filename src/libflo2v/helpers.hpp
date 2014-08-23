7be3f291ea167e1ba9e53ea513ed08dee2e6a6f8
Howard Mao <zhehao.mao@gmail.com>
2014-08-22 17:23:17 -0700

    make name generation simpler by getting rid of first section of multi-section name

--- src/libflo2v/helpers.hpp
+++ src/libflo2v/helpers.hpp
@@ -33,22 +33,21 @@ const std::string class_name(std::shared_ptr<flo<node, operation<node> > > &flof
 const std::string node_name(nodeptr node)
 {
     std::string name = node->name();
-    std::string norm_name = "";
+    std::vector<std::string> sections;
     size_t last_index = 0;
 
     // Constants with known widths should have the width specified
-    if (node->known_width() && node->is_const()) {
+    if (node->known_width() && node->is_const())
         return std::to_string(node->width()) + "'d" + node->name();
-    }
 
     // If there are any colons or double colons, replace them with underscores
     while (true) {
         size_t index = name.find(":", last_index);
         if (index == std::string::npos) {
-            norm_name += name.substr(last_index);
+            sections.push_back(name.substr(last_index));
             break;
         }
-        norm_name += name.substr(last_index, index - last_index) + "_";
+        sections.push_back(name.substr(last_index, index - last_index) + "_");
         if (index + 1 < name.length() && name[index + 1] == ':') {
             // if it's a double colon, skip both of them
             last_index = index + 2;
@@ -56,6 +55,20 @@ const std::string node_name(nodeptr node)
             last_index = index + 1;
         }
     }
+
+    // If it is a simple name with one section, just return it
+    if (sections.size() == 1)
+        return sections[0];
+
+    std::vector<std::string>::iterator iter = sections.begin();
+    // ignore the first section
+    iter++;
+    // connect the sections with underscores
+    std::string norm_name = *iter;
+    iter++;
+    for ( ; iter != sections.end(); iter++)
+        norm_name += "_" + *iter;
+
     return norm_name;
 }
 
