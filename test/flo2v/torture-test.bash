3c03ea8777905bfe80f02f40a1f34139a3b8e713
Howard Mao <zhehao.mao@gmail.com>
2014-08-26 14:19:58 -0700

    start work on torture test program

---
+++ test/flo2v/torture-test.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+FLO2V="$PWD/bin/flo2v"
+STEP2TB="$PWD/bin/step2tb"
+
+trap "rm -f *.vcd *.v *.step *.flo" EXIT
+
+for i in {0..20}; do
+    rm -f *.vcd *.v *.step *.flo
+    rm -rf torture torture.daidir
+    flo-torture --seed "$RANDOM"
+    $FLO2V Torture.flo > Torture.v
+    vcd2step Torture.vcd Torture.flo Torture.step
+    $STEP2TB Torture.step Torture.flo > Torture_tb.v
+    vcs -full64 -q -o torture -Mupdate Torture_tb.v Torture.v > /dev/null
+    ./torture > /dev/null
+    #vcddiff Torture.vcd Torture-test.vcd
+done
+
+
+echo "Test passed"
